036c0044aeaa1f39dd79f20d12838ecd4b2b07aa
Yash Goswami <yashgoswami@Yashs-MacBook-Pro.local>
2022-06-11 23:48:18 +0530

    mui datatable

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,7 +1,18 @@
+import Sidebar from '../../components/Sidebar/Sidebar';
+import Navbar from '../../components/Navbar/Navbar';
+import Datatable from '../../components/Datatable/Datatable';
 import './List.scss';
 
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <Datatable />
+      </div>
+    </div>
+  );
 };
 
 export default List;
